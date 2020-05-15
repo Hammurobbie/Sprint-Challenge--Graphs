5544377dede0547edbe3a411423fd3b76dd117f2
Robbie Porter <rthomporter@gmail.com>
2020-05-15 01:17:10 -0500

    cur path approach that isn't working

--- adv.py
+++ adv.py
@@ -12,8 +12,8 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
+#map_file = "maps/test_cross.txt"
+map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
 
@@ -29,6 +29,7 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+cur_path = []
 
 
 class Graph:
@@ -44,6 +45,7 @@ class Graph:
 
         while s.size() > 0:
             cur_room = s.pop()
+            print("cur room", cur_room)
 
             if cur_room not in visited:
                 visited.add(cur_room)
@@ -57,35 +59,69 @@ class Graph:
                     if player.current_room.get_room_in_direction(exit_boi).id not in s.stack and player.current_room.get_room_in_direction(exit_boi).id not in visited:
                         s.push(
                             player.current_room.get_room_in_direction(exit_boi).id)
-                        # new_room = player.current_room.id
-                        # s.push(new_room)
                 print("stack", s.stack)
                 if s.stack:
                     next_room = s.stack[-1]
                     print("next room", next_room)
+                    next_rooms_arr = []
                     for exit_boi2 in exits:
+                        print("exit for next", exit_boi2)
+                        next_rooms_arr.append(
+                            player.current_room.get_room_in_direction(exit_boi2).id)
                         if player.current_room.get_room_in_direction(exit_boi2).id == next_room:
                             player.travel(exit_boi2)
                             traversal_path.append(exit_boi2)
+                            cur_path.append(exit_boi2)
+                            print("cur path", cur_path)
+                    while set(next_rooms_arr).issubset(visited):
+                        print("next rooms ar", next_rooms_arr, visited)
+                        if cur_path:
+                            last_move = cur_path.pop()
+                            # print("cur path", cur_path)
+                            if last_move == "n":
+                                backtrack = "s"
+                            elif last_move == "s":
+                                backtrack = "n"
+                            elif last_move == "e":
+                                backtrack = "w"
+                            elif last_move == "w":
+                                backtrack = "e"
+                            player.travel(backtrack)
+                            traversal_path.append(backtrack)
+                            print("currr room exits",
+                                  player.current_room.get_exits())
+                            next_rooms_arr.pop()
+                            exits2 = player.current_room.get_exits()
+                            for exit_boi3 in exits2:
+                                next_rooms_arr.append(
+                                    player.current_room.get_room_in_direction(exit_boi3).id)
+                                if player.current_room.get_room_in_direction(exit_boi3).id == next_room:
+                                    print("what the fuckjttttttttttttttt",
+                                          player.current_room.id)
+                                    player.travel(exit_boi3)
+                                    traversal_path.append(exit_boi3)
+                                    print("what the fuckjttttttttttttttt",
+                                          player.current_room.id)
+                        else:
+                            # for exit_boi2 in exits:
+                            #     if player.current_room.get_room_in_direction(exit_boi2).id == next_room:
+                            #         player.travel(exit_boi2)
+                            #         traversal_path.append(exit_boi2)
+                            #         cur_path.append(exit_boi3)
+                            #         print(
+                            #             "what the fuckjkkkkkkkkkkkkkkk", cur_path)
+                            if exit_boi3 == "n":
+                                cur_path.append("s")
+                            elif exit_boi3 == "s":
+                                cur_path.append("n")
+                            elif exit_boi3 == "e":
+                                cur_path.append("w")
+                            elif exit_boi3 == "w":
+                                cur_path.append("e")
+                            break
 
-    # def dft_recursive(self, starting_vertex, visited=None):
 
-    #     exits = player.current_room.get_exits()
-
-    #     if visited is None:
-    #         visited = set()
-
-    #     visited.add(starting_vertex)
-    #     print("start point", starting_vertex)
-
-    #     for exit_boi in exits:
-    #         if exit_boi not in visited:
-    #             player.travel(exit_boi)
-    #             new_room = player.current_room.id
-    #             self.dft_recursive(new_room, visited)
-
-
-Graph().dft(player.current_room.id, )
+Graph().dft(player.current_room.id)
 
 
 print("player end room", player.current_room.id)
